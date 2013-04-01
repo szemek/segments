bb9afc0f31143b9676c4d8ee873b8d3bde769f2b
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2013-04-01 22:08:25 +0200

    basic views structure

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,17 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.Application.index("Your new application is ready."))
   }
-  
+
+  def capture(id: Long) = Action {
+    Ok(views.html.Application.capture())
+  }
+
+  def display(id: Long) = Action {
+    Ok(views.html.Application.display())
+  }
+
 }
